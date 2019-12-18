d8400fffc940c44c7bfa8d12f2a65de5966d3d9b
OmegaPointZero <joshua.erik.campbell@gmail.com>
2019-12-18 14:00:32 -0700

    can parse lexed tokens into sentences

--- lexer.py
+++ lexer.py
@@ -4,8 +4,9 @@ import re
 
 parser = argparse.ArgumentParser(description='Lexical analyzer for Lingr')
 
-parser.add_argument('-f, --file', action='store', dest='file_path', help='Specify a single file to work with')
-parser.add_argument('-p, --printlex', action='store_true', help='Print the lexed AST')
+parser.add_argument('-f, --file', action='store', dest='file_path', help='Specify a single file to work with, or comma seperated files')
+parser.add_argument('-l, --printlex', action='store_true', dest='printlex', help='Print the lexed lexed token list')
+parser.add_argument('-a, --printast', action='store_true', dest='printast', help='Print the Abstract Syntax Tree')
 
 results = parser.parse_args()
 
@@ -17,15 +18,15 @@ def tokenize(token):
     if token[0] in start_identifiers:
         return[{'TYPE' : 'IDENTIFIER', 'TOKEN': token[0]}] + tokenize(token[1:])
     elif token[-1] in end_identifiers:
-        return tokenize(token[:-1]) + [ { 'TYPE' : 'IDENTIFIER', 'TOKEN' : token[-1] } ]
+        return tokenize(token[:-1]) + [{ 'TYPE' : 'IDENTIFIER', 'TOKEN' : token[-1] }]
     if token[0] == '_':
-        return [ { 'TYPE': 'NAME', 'TOKEN': token } ]
+        return [{ 'TYPE': 'NAME', 'TOKEN': token }]
     elif re.match('^\d+$', token):
         return [{'TYPE' : 'INT', 'TOKEN': token}]
     elif token in operators:
-        return [ { 'TYPE' : 'OPERATOR', 'TOKEN' : token} ]
+        return [{ 'TYPE' : 'OPERATOR', 'TOKEN' : token}]
     else:
-        return [ { 'TYPE' : 'ITEM', 'TOKEN' : token } ]
+        return [{ 'TYPE' : 'ITEM', 'TOKEN' : token }]
 
 def lex_line(line):
     items = line.split(' ')
@@ -64,12 +65,30 @@ def parse_file(path):
         for lex in lexed:
             file_lexed.append(lex)
     stringified = identify_strings(file_lexed)
-    for s in stringified:
-        print(s)
+    return stringified
 
-parse_file(results.file_path);
+def sentenced(tokens):
+    sentences = []
+    this_sentence = []
+    for t in tokens:
+        if t['TYPE'] == 'IDENTIFIER' and t['TOKEN'] == ';':
+            sentences.append(this_sentence)
+            this_sentence = []
+        else:
+            this_sentence.append(t)
+    return sentences
 
+def generateAST(tokens):
+    # A sentence is a line of code. Sentences are demarcated by the ';' symbol.
+    sentences = sentenced(tokens)
+    print('Sentences:')
+    for sentence in sentences:
+        print(sentence)
 
-
-
-
+def main(options):
+    lexed_items = parse_file(options.file_path)
+    if options.printlex == True:
+        for s in lexed_items:
+            print(s)
+    ast = generateAST(lexed_items)
+main(results)
