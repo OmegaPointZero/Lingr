d407d6b4df47516e528f50cf8387cfe402c26b10
OmegaPointZero <joshua.erik.campbell@gmail.com>
2019-12-25 14:15:37 -0700

    more work on parsing order of operations, starting with only parenthesis

--- ast.py
+++ ast.py
@@ -1,5 +1,7 @@
 import lexer
 import argparse
+import re
+
 
 class expressions:
     def __init__(self, phrase):
@@ -9,17 +11,21 @@ class expressions:
     def add(self):
         self.self = self
         phrase = self.phrase
-        print('working phrase: %s' % phrase)
-        addable = ['INT', 'STRING', 'FLOAT', 'ARRAY']
-        first = phrase[0]
-        operator = phrase[1]
-        second = phrase[2]
-        if(first['TYPE'] in addable and second['TYPE'] in addable):
-            return "ADD "+first['TOKEN']+" "+second['TOKEN']
-        elif(first['TYPE'] in addable and second['TYPE'] == 'RESOLVABLE'):
-            return "ADD "+first['TOKEN']+" RESOLVABLE "
-        else: return None
 
+        addable = ['INT', 'STRING', 'FLOAT', 'ARRAY']
+        if len(phrase) == 0 or len(phrase) == 1 or len(phrase) == 2:
+            return None
+        elif len(phrase) == 3:
+            first = phrase[0]
+            operator = phrase[1]
+            second = phrase[2]
+            if(first['TYPE'] in addable and second['TYPE'] in addable):
+                return "ADD "+first['TOKEN']+" "+second['TOKEN']
+            elif(first['TYPE'] in addable and second['TYPE'] == 'RESOLVABLE'):
+                return "ADD "+first['TOKEN']+" RESOLVABLE "+second['TOKEN']
+            else: return None
+        else: 
+            return None
  
 
     def lookup_phrase(self):
@@ -46,35 +52,41 @@ class AST:
         tokens = self.token_list
         tree = []
         exp = [] # expression being built
-        nested = [] # things contained within parenthesis
-        nesting = 0 # how far nested we are right now
-        n_count = 0 # the number of nested items
-        for token in tokens:
-            if(len(exp)>2):
-                phrase = expressions(exp).lookup_phrase()
-                print('HERE IS THE PHRASE!! ######### LOOK HERE #########')
-                print(phrase)
+        resolvables = [] 
+        resolvable_count = 0 
+        current_count = 0
+        resolvable_nest = []
+        for token in tokens: 
+            print('token: %s' % token)
+            phrase = expressions(exp).lookup_phrase()
+            if phrase == None:
+                print('No Phrase!')
                 if self.tokeneval(token,'IDENTIFIER',';'): 
-                    print('End of line detected')
-                    if phrase == None:
-                        print('Abstract Syntax Table ERROR: %s\nNO SUCH EXPRESSION FOUND' % exp)
-                        exp = []
-                        break
+                    print('Abstract Syntax Table ERROR: %s\nNO SUCH EXPRESSION FOUND' % exp)
+                    exp = []
+                    break
+                elif self.tokeneval(token,'IDENTIFIER','(') == True:
+                    exp.append({'TYPE' : 'RESOLVABLE', 'TOKEN' : 'RESOLVABLE'+str(resolvable_count)})
+                    resolvable_nest.append(resolvable_count)
+                    resolvable_count += 1
+                    current_count += 1
+                elif self.tokeneval(token,'IDENTIFIER',')') == True:
+                    exp.append({'TYPE': 'RESOLVABLE', 'TOKEN' : 'END'+str(resolvable_nest[current_count])}) 
+                    current_count -= 1
+                    resolvable_nest.pop()
+                else:
+                    exp.append(token)
+            elif re.match('/^RESOLVABLE/', phrase.split(' ')[-1]):
+                """ writing an AST and lexer is really,  really hard """
+            else:
+                print('phrase: %s' % phrase)
+                if self.tokeneval(token,'IDENTIFIER',';'): 
+                    if(exp == []):
+                        print('End of line detected')
                     else:
                         tree = tree + exp
                         exp = []
-                if self.tokeneval(token,'IDENTIFIER','(') == True:
-                    exp.append({'TYPE' : 'RESOLVABLE', 'TOKEN' : 'RESOLVABLE'+str(nesting)})
-                    nesting += 1 
-                    n_count += 1
-                    nested.insert(0,exp)
-                elif self.tokeneval(token,'IDENTIFIER',')') == True:
-                    print("FOUND A CLOSING BRACKET!")
-                else:
-                    exp.append(token)
-            else:
-                exp.append(token) 
-
+        print(tree)
         return tree
 
 ast = AST(lexer.main(argparse.Namespace(file_path='test.lr', printast=True, printlex=False, standalone=False)))
